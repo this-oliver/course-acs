b7e910f5eb1fd949f3025fca4bff7d3a4914ff83
olivermanzi <olivermanzi.dev@gmail.com>
2021-09-16 12:23:16 +0200

    excersise 3 complete

--- lab_2/RSA/RSA.py
+++ lab_2/RSA/RSA.py
@@ -14,13 +14,11 @@ from cryptography.hazmat.primitives import serialization
 
 # Methods for encryption/decryption
 
-def simple_rsa_encrypt(m, publickey):
-    numbers = publickey.public_numbers()
-    return gmpy2.powmod(m, numbers.e, numbers.n)
+def simple_rsa_encrypt(message, exponent, number):
+    return gmpy2.powmod(message, exponent, number)
 
-def simple_rsa_decrypt(c, privatekey):
-    numbers = privatekey.private_numbers()
-    return gmpy2.powmod(c, numbers.d, numbers.public_numbers.n)
+def simple_rsa_decrypt(cipher, exponent, number):
+    return gmpy2.powmod(cipher, exponent, number)
 
 def int_to_bytes(i):
     # i might be a gmpy2 big integer; convert back to a Python int
@@ -33,10 +31,13 @@ def bytes_to_int(b):
 # We start by generatin the keys, the private key is kept secret
 # and the publi key is given to peers communicated with.
 
+key_length = 4096
+exponent = 65537
+
 # Generate a private key.
 private_key = rsa.generate_private_key(
-     public_exponent=65537,
-     key_size=2048,
+     public_exponent=exponent,
+     key_size=key_length,
      backend=default_backend()
 )
 
@@ -67,21 +68,22 @@ public_key = serialization.load_pem_public_key(
     public_key_bytes,
     backend=default_backend())
 
-print(private_key_bytes)
-print(public_key_bytes)
-
+print("\n === key length ===\n", key_length)
+print("\n === public exponent ===\n", exponent)
+print("\n === private key ===\n\n", private_key_bytes)
+print("\n === public key ===\n\n", public_key_bytes)
 
 # THIS IS JUST FOR TRAINING (DO NOT USE THIS FOR REAL CRYPTOGRAPHY)
 
 message = input("\nPlaintext: ").encode()
-
-# Now let's encrypt the message using the publi key
 message_as_int = bytes_to_int(message)
-cipher_as_int = simple_rsa_encrypt(message_as_int, public_key)
-cipher = int_to_bytes(cipher_as_int)
-print("The encrypter message looks like this :", cipher)
 
-#Let's see if we can get the text back
-message_as_int = simple_rsa_decrypt(cipher_as_int, private_key)
+## ENCRYPTING WITH PUBLIC KEY
+cipher_as_int = simple_rsa_encrypt(message_as_int, public_key.public_numbers().e, public_key.public_numbers().n)
+cipher = int_to_bytes(cipher_as_int)
+print("\n === Encrypted message ===\n", cipher)
+
+# Let's see if we can get the text back
+message_as_int = simple_rsa_decrypt(cipher_as_int, private_key.private_numbers().d, private_key.private_numbers().public_numbers.n)
 message = int_to_bytes(message_as_int)
-print("\nDecrypted messsage: {}\n".format(message))
+print("\n === Decrypted messsage: {}\n".format(message))
---
+++ lab_2/RSA/RSA_Reverse.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 17:44:43 2021
+
+@author: christer
+"""
+
+import gmpy2
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+
+# Methods for encryption/decryption
+
+def simple_rsa_encrypt(message, exponent, number):
+    return gmpy2.powmod(message, exponent, number)
+
+def simple_rsa_decrypt(cipher, exponent, number):
+    return gmpy2.powmod(cipher, exponent, number)
+
+def int_to_bytes(i):
+    # i might be a gmpy2 big integer; convert back to a Python int
+    i = int(i)
+    return i.to_bytes((i.bit_length()+7)//8, byteorder='big')
+
+def bytes_to_int(b):
+    return int.from_bytes(b, byteorder='big')
+
+# We start by generatin the keys, the private key is kept secret
+# and the publi key is given to peers communicated with.
+
+key_length = 4096
+exponent = 65537
+
+# Generate a private key.
+private_key = rsa.generate_private_key(
+     public_exponent=exponent,
+     key_size=key_length,
+     backend=default_backend()
+)
+
+# Extract the public key from the private key.
+public_key = private_key.public_key()
+
+# Convert the private key into bytes. We won't encrypt it this time.
+private_key_bytes = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+# Convert the public key into bytes.
+public_key_bytes = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+# Convert the private key bytes back to a key.
+# Because there is no encryption of the key, there is no password.
+private_key = serialization.load_pem_private_key(
+    private_key_bytes,
+    backend=default_backend(),
+    password=None)
+
+public_key = serialization.load_pem_public_key(
+    public_key_bytes,
+    backend=default_backend())
+
+print("\n === key length ===\n", key_length)
+print("\n === public exponent ===\n", exponent)
+print("\n === private key ===\n\n", private_key_bytes)
+print("\n === public key ===\n\n", public_key_bytes)
+
+# THIS IS JUST FOR TRAINING (DO NOT USE THIS FOR REAL CRYPTOGRAPHY)
+
+message = input("\nPlaintext: ").encode()
+message_as_int = bytes_to_int(message)
+
+### ENCRYPTING WITH PRIVATE KEY
+cipher_as_int = simple_rsa_encrypt(message_as_int, private_key.private_numbers().d, private_key.private_numbers().public_numbers.n)
+cipher = int_to_bytes(cipher_as_int)
+print("\n === Encrypted message ===\n", cipher)
+
+# Let's see if we can get the text back
+message_as_int = simple_rsa_decrypt(cipher_as_int, public_key.public_numbers().e, public_key.public_numbers().n)
+message = int_to_bytes(message_as_int)
+print("\n === Decrypted messsage: {}\n".format(message))
