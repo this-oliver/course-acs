7959c6b10140d380741823bd893c5b015b9f046e
olivermanzi <olivermanzi.dev@gmail.com>
2021-09-17 12:55:25 +0200

    prints sha1 and sha256 hashes

--- lab_2/Hash.py
+++ lab_2/Hash.py
@@ -23,16 +23,8 @@ print("Message: ", message_to_hash.decode('UTF-8'))
 md5_hasher = hashlib.md5(message_to_hash)
 print("MD5 hash: ", md5_hasher.hexdigest())
 
-md5_hasher_salt = hashlib.md5(salt)
-print("MD5 hash salt: ", md5_hasher_salt.hexdigest())
-
-md5_hasher_res = hashlib.md5(message_to_hash+salt)
-print("MD5 hash result: ", md5_hasher_res.hexdigest())
-
-"""
 sha1_hasher= hashlib.sha1(message_to_hash)
 print("SHA1 hash: ", sha1_hasher.hexdigest())
 
 sha256_hasher= hashlib.sha256(message_to_hash)
 print("SHA256 hash: ",sha256_hasher.hexdigest())
-"""
