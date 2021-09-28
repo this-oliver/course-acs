44148f6af871b99accb610c21a0f55cb055c9262
olivermanzi <olivermanzi.dev@gmail.com>
2021-09-28 14:36:47 +0200

    refactors mfa

--- lab_3/MFA.py
+++ lab_3/MFA.py
@@ -14,58 +14,63 @@ import qrcode
 multiFactorAuthEnabled = False
 
 # See this as a local database hosting user and password
-correctUsername = "enter the login name of your choice"
-correctPassword = "enter a password tht you like to use"
+correctUsername = "jakoli123"
+correctPassword = "jakoli123"
 #########################################################
 
-#Functionn for verifying username and password
+# Functionn for verifying username and password
 def login():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
 
-    if username == correctUsername and password == correctPassword: 
+    if username == correctUsername and password == correctPassword:
         return True
     else:
         return False
 
 
 # Enables multifactor authentication
-def registerMFA():      
-   global t, multiFactorAuthEnabled
- 
-   t=pyotp.TOTP("AnInitialSecrecy") 
-   auth_str=t.provisioning_uri(name='MG@AppliedComputerSecurity', issuer_name='AppliedComputerSecurity')
-   img=qrcode.make(auth_str)
-   plt.imshow(img)
-   plt.show()
-   input("Scan the QR-code with Google Authenticator and press Enter") 
-   multiFactorAuthEnabled = True
+def registerMFA():
+    global t, multiFactorAuthEnabled
+
+    t = pyotp.TOTP("AnInitialSecrecy")
+    auth_str = t.provisioning_uri(
+        name="MG@AppliedComputerSecurity", issuer_name="AppliedComputerSecurity"
+    )
+    img = qrcode.make(auth_str)
+    plt.imshow(img)
+    plt.show()
+    input("Scan the QR-code with Google Authenticator and press Enter")
+    multiFactorAuthEnabled = True
+
 
 # The menu takes your choice and enables you to login with or without MFA
 def menu():
-    alt = input("""
-                      A: Register for Multifactor Authentication 
-                      B: Login
-                      Q: Quit
-                     Enter your choice: """)
-    if alt == "A" or alt =="a":
+    alt = input(
+        """ 
+        A: Register for Multifactor Authentication 
+        B: Login
+        Q: Quit
+        Enter your choice: """
+    )
+
+    if alt == "A" or alt == "a":
         registerMFA()
-    elif  alt == "B" or alt =="b":
-        if login()==False: 
+    elif alt == "B" or alt == "b":
+        if login() == False:
             print("\nNo such user or the password is incorrect!")
         elif multiFactorAuthEnabled == True:
-                multiFactorAuth = input("Enter code: ")
-                if multiFactorAuth == t.now():
-                    print("\nThe login was successfull!")
-                else:
-                    print("\nWrong authentication code entered!")
+            multiFactorAuth = input("Enter code: ")
+            if multiFactorAuth == t.now():
+                print("\nThe login was successfull!")
+            else:
+                print("\nWrong authentication code entered!")
         else:
-            print("\nThe login was successfull!")                        
+            print("\nThe login was successfull!")
     elif alt == "Q" or alt == "q":
         sys.exit()
-    menu()
 
-    
-print("!!!!!!!!!!!!! Welcome to a MF-Authentication Demo !!!!!!!!!!!!!")
-print()
-menu()
+
+print("!!!!!!!!!!!!! Welcome to a MF-Authentication Demo !!!!!!!!!!!!!\n")
+while True:
+    menu()
